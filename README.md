1441ff0c847c8f701522cd7e53c4930c6b104119
Jose <josellausas@gmail.com>
2013-12-03 12:24:09 -0600

    Modified README.md and added some comments

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-luaColors
+luaColors by jose@josellausas.com
 =========
 
 Output nice colors to the terminal using lua. This module also overrides the print() function to something more useful.
+
+Use inside the print() function to turn off/on text formatting and colors. 
+
+Example:
+1. Turns off previous format. 
+2. Turns on blue color. 
+3. Turns on white background.
+
+local lc = require(“luaColors”)
+print(lc.off .. lc.blue .. lc.bg.white .. “Hello World” .. lc.off);
+
--- main.lua
+++ main.lua
@@ -1,10 +1,20 @@
+----------------------------------------
+-- main.lua by jose@josellausas.com
+-- created on: Dec 3, 2013
 -- This is a sample usage of luaColors
 
+-- Require the luaColors module and use is as lc
 local lc = require("luaColors")
 
--- lc.off turns off the current color formatting
-
+-- The standard print that has now been overrided with a colorful one:
 print("Hello luaColors")
+
+-- Other examples:
+-- Turns off current format and sets the color to blue
 print(lc.off .. lc.blue .. "Hello Blue!" .. lc.off)
+
+-- Sets the background to magenta and color to white, then turns off format
 print(lc.bg.magenta .. lc.white .. "Hello Background!" .. lc.off)
+
+-- Sets the format to blink text
 print(lc.blink .. "Bye bye!" .. lc.off)
